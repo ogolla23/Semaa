3c4a41977dc5d3c6e6554945aa3ee3cae9489bea
Leonard Ogolla <94747988+ogolla23@users.noreply.github.com>
2025-03-12 17:39:40 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript to handle the signup button click
+document.getElementById('signupBtn').addEventListener('click', function() {
+    // Replace the URL below with your Google Form URL
+    window.open('https://docs.google.com/forms/d/e/1FAIpQLSfCDkXSq8ZYSlzyl2rhp4sH1LFGJO4XLU1GjeGyyLXVaMfnTQ/viewform?usp=sharing', '_blank');
+});
